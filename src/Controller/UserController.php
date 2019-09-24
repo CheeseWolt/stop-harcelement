c8f42ac2b411c368f561e0816c28e733ce29fbc7
Benoit Routier <benoit.routier7@gmail.com>
2019-09-24 18:23:19 +0200

    Encoded password in user_edit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -65,13 +65,15 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, User $user): Response
+    public function edit(Request $request, User $user,  UserPasswordEncoderInterface $encoder): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) 
         {
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('user_index');
