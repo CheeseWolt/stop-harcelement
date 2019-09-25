27599e14353f15b71bdd9d94e6771ac96e9123e7
Benoit <benoit.routier7@gmail.com>
2019-09-25 15:50:19 +0200

    Added security (for real)

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -14,15 +14,12 @@ class DashboardController extends AbstractController
     public function index(AlertRepository $alertRepository)
     {
         $alerts = $alertRepository->getStatusRatioByMonth();
-        if (isset($alert)) {
             foreach ($alerts as $alert) {
                 $dtv[$alert['idAlert']][$alert['mois']] = $alert['nb'];
             }
-        }
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
             'dtv' => $dtv??null,
-            'alert'=> $alerts??null
         ]);
     }
 }
