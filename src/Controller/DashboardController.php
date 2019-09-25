ca08cd12a86bd12dbd7e694976ea8b4a507d1a20
Benoit <benoit.routier7@gmail.com>
2019-09-25 16:11:41 +0200

    Added dynamic scale to graph

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -13,13 +13,18 @@ class DashboardController extends AbstractController
      */
     public function index(AlertRepository $alertRepository)
     {
+        $dtvMax = 0;
         $alerts = $alertRepository->getStatusRatioByMonth();
             foreach ($alerts as $alert) {
                 $dtv[$alert['idAlert']][$alert['mois']] = $alert['nb'];
+                if ($dtvMax < $alert['nb']) {
+                    $dtvMax = $alert['nb'];
+                }
             }
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
             'dtv' => $dtv??null,
+            'dtvMax'=> $dtvMax
         ]);
     }
 }
