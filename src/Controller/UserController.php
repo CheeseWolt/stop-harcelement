dd724496284277dec06da33107c627edbee52380
Benoit Routier <46683196+CheeseWolt@users.noreply.github.com>
2019-09-27 23:09:22 +0200

    Fixed user/edit to match role select restriction

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -83,15 +83,34 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, User $user,  UserPasswordEncoderInterface $encoder): Response
+    public function edit(Request $request, User $user,  UserPasswordEncoderInterface $encoder, RoleRepository $roleRepository): Response
     {
-        $form = $this->createForm(UserType::class, $user);
+        $Admin = new Role();
+        $Prof = new Role();
+        $Eleve = new Role();
+        $Secretariat = new Role();
+        $roles['Professeur'] = $Prof->setName('ROLE_PROFESSEUR');
+        $roles['Élève'] = $Eleve->setName('ROLE_ELEVE');
+        if ($this->getUser()->getRoles()[0] == 'ROLE_ADMIN') {
+            $roles['Admin'] = $Admin->setName('ROLE_ADMIN');
+            $roles['Secrétariat'] = $Secretariat->setName('ROLE_SECRETARIAT');
+        }
+        $form = $this->createForm(UserType::class, $user)
+            ->add('role', ChoiceType::class, [
+                'choices' => $roles,
+                'label' => 'name'
+            ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $role = $form['role']->getData();
+            $role = $roleRepository->findBy(['name'=>$role->getName()]);
+            $user->setRole($role[0]);
             $hash = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($hash);
-            $this->getDoctrine()->getManager()->flush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
 
             return $this->redirectToRoute('user_index');
         }
