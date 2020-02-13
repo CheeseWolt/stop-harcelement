991fdce3dad082cbc8cfed347269df458e5c4436
Benoit <benoit.routier7@gmail.com>
2020-02-13 11:18:49 +0100

    added sql starter

---
+++ setup.sql
@@ -0,0 +1,4 @@
+insert into sex values(null,"Masculin"),(null,"Féminin"),(null,"Autre");
+insert into role values(null,"ROLE_ADMIN"),(null,"ROLE_SECRETARIAT"),(null,"ROLE_PROFESSEUR"),(null,"ROLE_ELEVE");
+insert into status values(null,"Témoin"),(null,"Victime");
+insert into user values(null,1,1,null,"Admin","Admin","Admin","$2y$10$8bawo6Y4UvG0.6ya1TXlr.bZetGn7NXPUcb1FL1HyU9uav6Ka3Rra",date(now()),null,"admin",null);
